5afb4a8190dd5b122be43fb1988060dd6df4034d
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-02-06 13:20:04 +0100

    Create exercise_2_with_solution.py
    Write a Python program to find out what version of Python you are using.
    
    Note : 'sys' module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter.

---
+++ Solutions/exercise_2_with_solution.py
@@ -0,0 +1,8 @@
+# Write a Python program to find out what version of Python you are using.
+# Note : 'sys' module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter.
+
+import sys
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
