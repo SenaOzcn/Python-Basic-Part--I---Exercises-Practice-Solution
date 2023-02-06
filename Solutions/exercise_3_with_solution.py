46881bcf748b5a3a13446f33f08a5da262c4b908
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-02-06 13:27:15 +0100

    Create exercise_3_with_solution.py
    The said code imports the "datetime" module, gets the current date and time, and finally prints it in a formatted string.
    
    1. The first line import datetime imports the datetime module which supplies classes for manipulating dates and times.
    2. The second line now = datetime.datetime.now() creates a datetime object for the current date and time.
    3. The third line print ("Current date and time : ") prints the string "Current date and time : " to the console.

---
+++ Solutions/exercise_3_with_solution.py
@@ -0,0 +1,12 @@
+import datetime
+now= datetime.datetime.now()
+print("Current date and time :")
+print(now)
+
+"""
+The said code imports the "datetime" module, gets the current date and time, and finally prints it in a formatted string.
+
+1. The first line import datetime imports the datetime module which supplies classes for manipulating dates and times.
+2. The second line now = datetime.datetime.now() creates a datetime object for the current date and time.
+3. The third line print ("Current date and time : ") prints the string "Current date and time : " to the console.
+"""
