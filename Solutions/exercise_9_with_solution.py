7c977bd0f9d1a91c5a29fdd0a601f2bf4c980cc5
Sena Nur Özcan <1.senaozcan@gmail.com>
2023-02-09 10:19:33 +0100

    Create exercise_9_with_solution.py

---
+++ Solutions/exercise_9_with_solution.py
@@ -0,0 +1,10 @@
+exam_st_date = (11,12,2014)
+print( "The examination will start from : %i / %i / %i"%exam_st_date)
+
+"""
+The said code creates a tuple called "exam_st_date" containing three integers: 11, 12, and 2014.
+It then uses string formatting to print a string that states "The examination will start from :
+"followed by the integers in the tuple in the order they appear, formatted as day/month/year. The placeholders %i are used to format the integers.
+
+The output would be: "The examination will start from : 11 / 12 / 2014".
+"""
